2bb6dd2825eef9b7078662832037e8ea03133482
rk <54451409+hanpec@users.noreply.github.com>
2023-08-08 00:28:30 +0900

    RSA暗号

---
+++ RSA暗号.py
@@ -0,0 +1,35 @@
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import serialization, hashes
+
+# 鍵の生成
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048
+)
+
+# 公開鍵の取得
+public_key = private_key.public_key()
+
+# メッセージの暗号化
+message = b"Hello, RSA!"
+ciphertext = public_key.encrypt(
+    message,
+    padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
+        label=None
+    )
+)
+
+# 復号化
+decrypted_message = private_key.decrypt(
+    ciphertext,
+    padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
+        label=None
+    )
+)
+
+print("Original Message:", message)
+print("Decrypted Message:", decrypted_message.decode('utf-8'))
