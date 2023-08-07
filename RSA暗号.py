41f6a6bbe11f82b9340261e9bc75e8af1b97da10
rk <54451409+hanpec@users.noreply.github.com>
2023-08-08 00:36:34 +0900

    python

--- RSA暗号.py
+++ RSA暗号.py
@@ -11,7 +11,7 @@ private_key = rsa.generate_private_key(
 public_key = private_key.public_key()
 
 # メッセージの暗号化
-message = b"This is a pan,pbn,pcn,pdn,pen!"
+message = b"python"
 ciphertext = public_key.encrypt(
     message,
     padding.OAEP(
